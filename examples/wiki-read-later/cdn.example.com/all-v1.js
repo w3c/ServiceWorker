2a1b19152694e13063069f14f1156db5c284d087
Jake Archibald <jaffathecake@gmail.com>
2013-06-19 14:58:58 +0100

    Don't want to init controller until first article taken offline

--- examples/wiki-read-later/cdn.example.com/all-v1.js
+++ examples/wiki-read-later/cdn.example.com/all-v1.js
@@ -1,4 +1,5 @@
 // set up the controller
 if (navigator.registerController) {
+  // TODO: this shouldn't happen until the user takes an article offline
   navigator.registerController("/*", "/ctrl.js");
 }
