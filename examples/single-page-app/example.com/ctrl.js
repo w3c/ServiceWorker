f2704409d25a837fc53698f32e7838b1681975c2
Jake Archibald <jaffathecake@gmail.com>
2013-09-26 17:37:25 +0100

    Implementation will be smart enough to pull fresh content from other caches

--- examples/single-page-app/example.com/ctrl.js
+++ examples/single-page-app/example.com/ctrl.js
@@ -6,33 +6,15 @@ var cacheNames = {
 
 // Install process
 this.oninstalled = function(event) {
-  var staticCache = new Cache();
-  var oldStaticCache;
-  // TODO: how can we avoid downloading both PNGs?
-  // https://github.com/slightlyoff/NavigationController/issues/60
-  var staticUrls = [
+  caches.set(cacheNames['static'], new Cache(
     '//cdn.example.com/all-v1.css',
     '//cdn.example.com/all-v1.js',
+    // TODO: how can we avoid downloading both PNGs?
+    // https://github.com/slightlyoff/NavigationController/issues/60
     '//cdn.example.com/whatever-v1.png',
     '//cdn.example.com/whatever-large-v1.png',
     '//cdn.example.com/whatever-v1.woff'
-  ];
-
-  if (event.previousVersion) {
-    oldStaticCache = caches.get('static' + event.previousVersion);
-  }
-
-  staticUrls.forEach(function(staticUrl) {
-    // copy from previous cache, if it's there
-    if (oldStaticCache && oldStaticCache.has(staticUrl)) {
-      staticCache.set(staticUrl, oldStaticCache.get(staticUrl));
-    }
-    else {
-      staticCache.add(staticUrl);
-    }
-  });
-
-  caches.set(cacheNames['static'], staticCache);
+  ));
 
   // core cache entried should be check on each controller update
   caches.set(cacheNames['core'], new Cache([
