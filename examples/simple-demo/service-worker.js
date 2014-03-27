64865089f1d820ef55fa2d3acb1734ab2e824b5c
Jeffrey Yasskin <jyasskin@google.com>
2014-03-27 16:43:10 -0700

    Add a simple echoing message handler to the service worker.

--- examples/simple-demo/service-worker.js
+++ examples/simple-demo/service-worker.js
@@ -19,3 +19,12 @@ onfetch = function(e) {
 		}));
 	}
 }
+
+onmessage = function(e) {
+	var source = e.source;
+	e.source.postMessage({chat: 'Received a message'});
+	var msg = e.data;
+	if (msg.hasOwnProperty('chat')) {
+		source.postMessage({chat: 'Echo: ' + msg.chat});
+	}
+}
