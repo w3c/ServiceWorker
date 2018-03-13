e605d639dc137d8bc522db2da3e0f0be209dd9c6
Yves Lafon <ylafon@w3.org>
2018-03-13 14:03:59 +0100

    avoid loop, need to stop rebuilds

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
+# avoid loop
+exit 0
+
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
