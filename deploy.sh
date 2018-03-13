6a8df1bb6fce0021d4fd82c25d0f0e993204d9f7
Yves Lafon <ylafon@w3.org>
2018-03-13 14:00:59 +0100

    fixing for only building/commiting spec, still more complex than needed

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 # From https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 
 SOURCE_BRANCH="master"
-TARGET_BRANCH="master"
 
 function doCompile {
   chmod 755 ./compile.sh
@@ -23,21 +22,16 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
+# Clone the existing branch for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO out
 cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-# Clean out existing contents
-rm -rf docs/index.html || exit 0
 
 # Run our compile script
 doCompile
 
 # Now let's go have some fun with the cloned repo
-cd docs
+
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -63,4 +57,4 @@ eval `ssh-agent -s`
 ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push $SSH_REPO 
