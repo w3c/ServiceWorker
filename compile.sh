42dec26976705184492f90cc439fef94ab0a7e70
Jake Archibald <jaffathecake@gmail.com>
2019-02-11 12:46:56 +0000

    Switching Travis to use Bikeshed rest API for quicker & simpler builds

--- compile.sh
+++ compile.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-cd docs && bikeshed spec
+INPUT_FILE=$(find . -maxdepth 2 -name "*.bs" -print -quit)
+
+curlretry() {
+    curl --retry 2 "$@"
+}
+
+curlbikeshed() {
+    # The Accept: header ensures we get the error output even when warnings are produced, per
+    # https://github.com/whatwg/whatwg.org/issues/227#issuecomment-419969339.
+    HTTP_STATUS=$(curlretry https://api.csswg.org/bikeshed/ \
+                            --output "$1" \
+                            --write-out "%{http_code}" \
+                            --header "Accept: text/plain, text/html" \
+                            -F die-on=error \
+                            -F file=@"$INPUT_FILE" \
+                            "${@:2}")
+
+    if [[ "$HTTP_STATUS" != "200" ]]; then
+        cat "$1"
+        rm -f "$1"
+        exit 22
+    fi
+}
+
+curlbikeshed "index.html"
 
 if [ -d out ]; then
   echo Copy the generated spec into out/index.html
